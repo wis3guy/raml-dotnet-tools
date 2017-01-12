e8daad184c14bfffbd4ffdb282e5282a5e8ab8fa
Pedro Wood <mail_pedro@yahoo.com.ar>
2017-01-12 10:23:54 -0300

    Incorrect detection of uses clause - #121

--- Raml.Common/RamlIncludesManager.cs
+++ Raml.Common/RamlIncludesManager.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -114,7 +115,8 @@ namespace Raml.Common
 
                 ManageInclude(lines, destinationFolder, includedFiles, path, relativePath, confirmOvewrite, i, scopeIncludedFiles, rootRamlPath, isRootFile, useLine);
 
-                if (lines[i].Contains("uses:") || lines[i].Contains("uses :"))
+                var usesRegex = new Regex("^uses\\s*:");
+                if (usesRegex.IsMatch(lines[i]))
                     useLine = true;
             }
 
